7b6f6ce35423ea433617f1dcfa700358988f5e69
MIZUNO Hiroki <mzpppp@gmail.com>
2008-05-12 22:39:39 +0900

    Change asm instruction conversion
    - make easy to add instruction

--- src/asm.ml
+++ src/asm.ml
@@ -1,5 +1,6 @@
 open Base
 
+(* public datatype *)
 type namespace = 
     Namespace of string 
   | PackageNamespace of string
@@ -20,7 +21,7 @@ type instruction =
   | CallPropLex of multiname * int
   | Pop
 
-(* constant list *)
+(* to create cpool *)
 type clist = {
   lint:    int list;
   luint:   int list;
@@ -37,16 +38,20 @@ type cmap = {
   uint:   int map;
   double: float map;
   string: string map;
-  namespace:     (namespace*Abc.namespace) map;
+  namespace: (namespace*Abc.namespace) map;
   namespace_set: (namespace_set*Abc.namespace_set) map;
-  multiname:     (multiname*Abc.multiname) map;
+  multiname: (multiname*Abc.multiname) map;
 }
 
-(* code *)
-type code = { bytes:Bytes.t list;
-	      stack:int;
-	      scope:int;
-	      count:int}
+(* instruction config *)
+type config = {
+  op:int;
+  args: cmap -> Bytes.t list;
+  const:  clist;
+  stack: int;
+  scope: int;
+  count: int;
+}
 
 type meth = {
   name: string;
@@ -58,16 +63,13 @@ type meth = {
   exceptions: int list;
 }
 
-let empty_clist = 
-  {lint=[]; 
-   luint=[]; 
-   ldouble=[]; 
-   lstring=[]; 
-   lnamespace=[]; 
-   lnamespace_set=[]; 
-   lmultiname=[]}
+let ns_name = 
+  function Namespace name | PackageNamespace name ->
+    name
 
+let const x _ = x
 
+(* clist *)
 let append_clist lhs rhs = 
   {lint           = lhs.lint @ rhs.lint;
    luint          = lhs.luint @ rhs.luint;
@@ -77,41 +79,16 @@ let append_clist lhs rhs =
    lnamespace_set = lhs.lnamespace_set @ rhs.lnamespace_set;
    lmultiname     = lhs.lmultiname @ rhs.lmultiname }
 
-let ns_name = 
-  function Namespace name | PackageNamespace name ->
-    name
-    
-let collect xs =
-  let f ({lstring=lstring;
-	  lint=lint;
-	  luint=luint;
-	  lnamespace=lns;
-	  lnamespace_set=lns_set;
-	  lmultiname=lname} as clist) = 
-    function
-	PushString str ->
-	  {clist with lstring=str::lstring} 
-      | PushInt n ->
-	  {clist with lint=n::lint}
-      | PushUInt n ->
-	  {clist with luint=n::luint}
-      | FindPropStrict name | CallPropLex (name,_) ->
-	  begin match name with
-	      QName (ns,str) ->
-		{clist with 
-		   lnamespace=ns::lns;
-		   lstring=(ns_name ns)::str::lstring;
-		   lmultiname=name::lname}
-	    | Multiname (string,ns_set) ->
-		{clist with
-		   lnamespace_set=ns_set::lns_set;
-		   lnamespace=(ns_set)@lns;
-		   lstring=(List.map ns_name ns_set)@lstring;
-		   lmultiname=name::lname } end
-      | _ -> 
-	  clist in
-    List.fold_left f empty_clist xs
+let empty_clist = 
+  {lint=[]; 
+   luint=[]; 
+   ldouble=[]; 
+   lstring=[]; 
+   lnamespace=[]; 
+   lnamespace_set=[]; 
+   lmultiname=[]}
 
+(* cmap *)
 let base_get =
   List.assoc
 
@@ -131,6 +108,18 @@ let of_multiname smap nsmap nssmap =
 	Abc.QName (complex_get ns nsmap,base_get s smap)
     | Multiname (s,nss) ->
 	Abc.Multiname (base_get s smap,complex_get nss nssmap)
+
+let str_get str {string=smap} = 
+  Bytes.U30 (base_get str smap)
+
+let int_get n {int=imap} = 
+  Bytes.U30 (base_get n imap)
+
+let uint_get n {uint=umap} = 
+  Bytes.U30 (base_get n umap)
+
+let name_get name {multiname=nmap} =
+  Bytes.U30 (complex_get name nmap)
 
 let cmap_of_clist {lint=int; 
 		   luint=uint;
@@ -165,7 +154,13 @@ let cmap_of_clist {lint=int;
      namespace_set=ns_set';
      multiname=name'}
 
-let cpool_of_cmap {int=int;uint=uint;double=double;string=string;namespace=ns;namespace_set=ns_set;multiname=name}= 
+let cpool_of_cmap {int=int;
+		   uint=uint;
+		   double=double;
+		   string=string;
+		   namespace=ns;
+		   namespace_set=ns_set;
+		   multiname=name}= 
   let base xs = 
     List.map fst xs in
   let complex xs =
@@ -180,6 +175,63 @@ let cpool_of_cmap {int=int;uint=uint;double=double;string=string;namespace=ns;na
     Abc.multiname=complex name
   }
 
+(* instruction configure *)
+let empty_config = {
+  op=0;
+  args=const [];
+  const= empty_clist;
+  stack=0;
+  scope=0;
+  count=0;
+}
+
+let int x = {
+  empty_clist with lint=[x]
+}
+
+let uint x = {
+  empty_clist with luint=[x]
+}
+
+let string x = {
+  empty_clist with lstring=[x]
+}
+
+let multiname name= 
+  match name with
+      QName (ns,str) ->
+	{empty_clist with 
+	   lnamespace=[ns];
+	   lstring=[ns_name ns;str];
+	   lmultiname=[name] }
+    | Multiname (str,ns_set) ->
+	{empty_clist with
+	   lnamespace_set=[ns_set];
+	   lnamespace=ns_set;
+	   lstring=str::List.map ns_name ns_set;
+	   lmultiname=[name] }
+
+let get_config = function
+    GetLocal ->
+      {empty_config with op=0xD0; stack=1}
+  | PushScope -> 
+      {empty_config with op=0x30; stack= ~-1; scope=1}
+  | ReturnVoid -> 
+      {empty_config with op=0x47}
+  | PushString str ->
+      {empty_config with op=0x2C; stack=1; const=string str; args=fun cmap -> [str_get str cmap];}
+  | PushInt n ->
+      {empty_config with op=0x2D; stack=1; const=int n; args=fun cmap -> [int_get n cmap];}
+  | PushUInt n ->
+      {empty_config with op=0x2E; stack=1; const=uint n; args=fun cmap -> [uint_get n cmap];}
+  | FindPropStrict name ->
+      {empty_config with op=0x5D; stack=1; const=multiname name; args=fun cmap ->[name_get name cmap];}
+  | Pop ->
+      {empty_config with op=0x29; stack= ~-1}
+  | CallPropLex(name,count) ->
+      {empty_config with op=0x4c; stack= ~-count; args=fun cmap->[name_get name cmap;Bytes.U30 count]}
+
+(* convert instruction *)
 let add (max,current) n = 
   let current' =
     current + n in
@@ -188,62 +240,20 @@ let add (max,current) n =
     else
       (max,current')
 
-let inc s = add s 1
-let dec s = add s ~-1
-
-let str_get str {string=smap} = 
-  Bytes.U30 (base_get str smap)
-
-let int_get n {int=imap} = 
-  Bytes.U30 (base_get n imap)
-
-let uint_get n {uint=umap} = 
-  Bytes.U30 (base_get n umap)
-
-let name_get name {multiname=nmap} =
-  Bytes.U30 (complex_get name nmap)
-
-let empty_code = {
-  bytes=[];
-  stack=0;
-  scope=0;
-  count=0 }
-
-(* convert instruction *)
-let encode cmap =
-  function 
-      GetLocal -> 
-	{empty_code with bytes=[Bytes.U8 0xD0]; stack=1}
-    | PushScope -> 
-	{empty_code with bytes=[Bytes.U8 0x30]; stack=(-1); scope=1}
-    | ReturnVoid -> 
-	{empty_code with bytes=[Bytes.U8 0x47]}
-    | PushString str ->
-	{empty_code with bytes=[Bytes.U8 0x2C;str_get str cmap]; stack=1}
-    | PushInt n ->
-	{empty_code with bytes=[Bytes.U8 0x2D;int_get n cmap]; stack=1}
-    | PushUInt n ->
-	{empty_code with bytes=[Bytes.U8 0x2E;uint_get n cmap]; stack=1}
-    | FindPropStrict name ->
-	{empty_code with bytes=[Bytes.U8 0x5D;name_get name cmap]; stack=1}
-    | Pop ->
-	{empty_code with bytes=[Bytes.U8 0x29]; stack=(-1)}
-    | CallPropLex(name,count) ->
-	{empty_code with 
-	   bytes=[Bytes.U8 0x4C;name_get name cmap;Bytes.U30 count]; 
-	   stack=(-count)}
-
 let method_asm cmap index m =
-  let codes =
-    List.map (encode cmap) m.instructions in
+  let configs =
+    List.map get_config m.instructions in
+  let init =
+    (0,0) in
   let (max_stack,_),(max_scope,_),local_count,bytes = 
-    List.fold_left 
+    List.fold_left
       (fun 
 	 (stack,scope,count,bytes) 
-	 {stack=st;scope=sc;count=c;bytes=by} -> 
-	   (add stack st,add scope sc,count+c,(List.rev by)@bytes))
-      ((0,0),(0,0),1,[])
-      codes in
+	 {op=op;args=args;stack=st;scope=sc;count=c} -> 
+	   let by =
+	     (Bytes.U8 op)::args cmap in
+	     add stack st,add scope sc,count+c,(List.rev by @bytes))
+      (init,init,1,[]) configs in
   let info =
     { Abc.params=m.params; 
       Abc.return=m.return; 
@@ -259,6 +269,10 @@ let method_asm cmap index m =
       Abc.exceptions=[]; 
       Abc.trait_m=[] } in
       info,body
+
+let collect xs =
+  List.fold_left append_clist empty_clist @@
+    List.map (fun x->(get_config x).const) xs
 
 let assemble methods =
   let cmap = 
