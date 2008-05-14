12aca9c0361b1346cfefd515f500e14b89b486d7
mzp <mzpppp@gmail.com>
2008-05-14 12:10:05 +0900

    Support negative-integer
    - Bytes module use int32

--- src/bytes.ml
+++ src/bytes.ml
@@ -3,16 +3,20 @@ type t =
     U8 of int 
   | U16 of int 
   | S24 of int 
-  | U30 of int 
-  | U32 of int
-  | S32 of int
+  | U30 of int32
+  | U32 of int32
+  | S32 of int32
   | D64 of float
 
 let u8 n = U8 n
 let u16 n = U16 n
-let u30 n = U30 n
-let u32 n = U32 n
-let s32 n = S32 n
+let u30 n = U30 (Int32.of_int n)
+let u32 n = U32 (Int32.of_int n)
+let s32 n = S32 (Int32.of_int n)
+
+let (&/) = Int32.logand
+let (|/) = Int32.logor
+let (>>) = Int32.shift_right_logical
 
 let of_int_list = function
     U8  x when x <= 0xFF -> 
@@ -20,17 +24,21 @@ let of_int_list = function
   | U16 x when x <= 0xFFFF -> 
       [x land 0xFF; (x lsr 8) land 0xFF ]
   | U30 x | U32 x | S32 x -> 
-      if x = 0 then
+      if x = 0l then
 	[0]
       else
 	unfold 
 	  (fun x -> 
-	   if x = 0 then 
+	   if x = 0l then 
 	     None
-	   else if 0 < x && x <= 0x7F then
-	     Some ((x land 0x7F),0)
-	   else
-	     Some ((x land 0x7F) lor 0x80,x lsr 7)) x
+	   else if 0l < x && x <= 0x7Fl then
+	     Some (Int32.to_int (x &/ 0x7Fl),0l)
+	   else 
+	     let next = 
+	       x >> 7 in
+	     let current =
+	       Int32.to_int ((x &/ 0x7Fl) |/ 0x80l) in
+	       Some (current,next)) x
   | _ ->
       invalid_arg "of_int_list"
 
