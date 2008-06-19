4e31801c3f8dead77063b3d98a9c67fc52bd6634
MIZUNO Hiroki <mzpppp@gmail.com>
2008-06-19 23:45:04 +0900

    Add example for recurtion (NOTE: not working NOW)

---
+++ example/rec.scm
@@ -0,0 +1,11 @@
+(let ([f (lambda (n) (print "not rec"))])
+  (let ([f (lambda (n) (if (= n 0)
+			   (print "rec")
+			   (f 0)))])
+    (f 1)))
+
+(let ([f (lambda (n) (print "not rec"))])
+  (letrec ([f (lambda (n) (if (= n 0)
+			   (print "rec")
+			   (f 0)))])
+    (f 1)))
