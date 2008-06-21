9e950e16651e5a43e30136b0df332f90cdee6c8b
MIZUNO Hiroki <mzpppp@gmail.com>
2008-06-21 13:47:24 +0900

    add rec example

--- example/rec.scm
+++ example/rec.scm
@@ -1,6 +1,7 @@
 ;; expected output
 ;;;  not rec
 ;;;  rec
+;;;  3628800
 (let ([f (lambda (n) (print "not rec"))])
   (let ([f (lambda (n) (if (= n 0)
 			   (print "rec")
@@ -12,3 +13,9 @@
 			   (print "rec")
 			   (f 0)))])
     (f 1)))
+
+(letrec ([fact (lambda (n) 
+		 (if (<= n 1)
+		     1
+		     (* n (fact (- n 1)))))])
+  (print (fact 10)))
