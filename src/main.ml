608ed609c498e7d16dd5ed862e2c209443de7117
mzp <mzpppp@gmail.com>
2008-10-28 19:11:29 +0900

    add -v option

--- src/main.ml
+++ src/main.ml
@@ -10,7 +10,6 @@ let generate path stream =
     Abc.to_bytes abc in
   let ch = 
     open_out_bin path in
-    print_endline path;
     Bytes.output_bytes ch bytes;
     close_out ch
 
@@ -21,13 +20,22 @@ let main () =
   let opt =
     OptParser.make () in
   let output =
-    StdOpt.str_option ~default:"a.abc" ~metavar:"OUTPUT" () in
+    StdOpt.str_option ~default:"a.abc" ~metavar:"<output>" () in
   let _ =
-    OptParser.add opt ~short_name:'o' ~long_name:"output" ~help:"output name" output in
+    OptParser.add opt ~short_name:'o' ~long_name:"output" 
+      ~help:"Set output file name to <file>" @@ output in
+  let version =
+    StdOpt.store_true () in
+  let _ = 
+    OptParser.add opt ~short_name:'v' ~long_name:"version" 
+      ~help:"Print compiler version and exit" @@ 
+      version in
   let inputs =
     OptParser.parse_argv opt in
-    if inputs = [] then
-     OptParser.usage opt ()
+    if get_option version then
+      print_endline "0.1.0"
+    else if inputs = [] then
+      OptParser.usage opt ()
     else begin
      let ch =
        open_in (List.hd inputs) in
