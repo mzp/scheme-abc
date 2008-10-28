68488e708411487a6956566483d720d0d27b40a5
mzp <mzpppp@gmail.com>
2008-10-28 20:07:31 +0900

    main

--- src/main.ml
+++ src/main.ml
@@ -18,23 +18,15 @@ let get_option x =
 
 let main () =
   let opt =
-    OptParser.make () in
+    OptParser.make ~version:"0.1.0" () in
   let output =
     StdOpt.str_option ~default:"a.abc" ~metavar:"<output>" () in
   let _ =
     OptParser.add opt ~short_name:'o' ~long_name:"output" 
       ~help:"Set output file name to <file>" @@ output in
-  let version =
-    StdOpt.store_true () in
-  let _ = 
-    OptParser.add opt ~short_name:'v' ~long_name:"version" 
-      ~help:"Print compiler version and exit" @@ 
-      version in
   let inputs =
     OptParser.parse_argv opt in
-    if get_option version then
-      print_endline "0.1.0"
-    else if inputs = [] then
+    if inputs = [] then
       OptParser.usage opt ()
     else begin
      let ch =
