30b066dd82eee05651e6473fb2b623955fbdfe2b
mzp <mzpppp@gmail.com>
2008-11-01 12:35:46 +0900

    [UPDATE] Class sample

--- example/class.scm
+++ example/class.scm
@@ -1,13 +1,18 @@
 ;; Example for class definition
+;;; 42
 ;;; 12
 ;;; [object Foo]
 ;;; 10
 
-(define-class Foo Object
-  ([init x] (let ((t 10)) 
-	      (let ((t 12))
-		(print t))))
-  ([f    x] (print x)))
+(define-class Foo (Object) ())
+(define-method init ((self Foo) x)
+  (print x)
+  (let ((t 10)) 
+    (let ((t 12))
+      (print t))))
+
+(define-method f ((self Foo) x)
+  (print x))
 
 (define foo (new Foo 42))
 (print foo)
