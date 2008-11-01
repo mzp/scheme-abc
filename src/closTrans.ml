84087f887697cc5f983df35a2abd4b817a30255b
mzp <mzpppp@gmail.com>
2008-11-01 10:48:49 +0900

    Add ClosTrans module.
    This is prototype. So, it does not have syntax, yet.

---
+++ src/closTrans.ml
@@ -0,0 +1,33 @@
+open Base
+
+type clos_stmt = 
+    Plain of Ast.stmt
+  | DefineClass  of string * Ast.name * string list
+  | DefineMethod of string * (string * string) * string list * Ast.expr
+
+(*
+  Features:
+  - convert DefineClass & DefineMethod to Ast.Class
+  - convert Ast.Call to Ast.Invoke
+*)
+
+let methods_table program =
+  let tbl =
+    Hashtbl.create 16 in
+    program +> List.iter
+      (function
+           DefineMethod (name,(self,klass),args,body) ->
+	     Hashtbl.add tbl klass (name,self::args,body)
+	 | _ ->
+	     ());
+    tbl
+
+let classize tbl program =
+  HList.concat_map 
+    (function
+	 Plain stmt ->
+	   [stmt]
+       | DefineClass (klass,super,_) ->
+	   [Ast.Class (klass,super,Hashtbl.find_all tbl klass)]
+       | DefineMethod _ ->
+	   []) program
