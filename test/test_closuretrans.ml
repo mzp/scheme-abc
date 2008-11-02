5928803e52b6764f6dddc8d0b8fdc67ff188cf5d
mzp <mzpppp@gmail.com>
2008-11-02 19:27:05 +0900

    [UPDATE]support closuer in define-method

--- test/test_closuretrans.ml
+++ test/test_closuretrans.ml
@@ -3,9 +3,21 @@ open Ast
 open Util
 open ClosureTrans
 
+let ok x y =
+  OUnit.assert_equal
+    ~printer:(fun x-> (Std.dump (List.map Ast.to_string_stmt x) ^ "\n"))
+    x y
+
 test closure_with_args =
-    OUnit.assert_equal
-      ~printer:(fun x-> (Std.dump (List.map Ast.to_string_stmt x)))
-      [Define ("f",Lambda (["x"],(Let (["x",Var "x"],Block [Lambda ([],Block [Var "x"])]))))]
-      (trans @@ compile_string "(define (f x) (lambda () x))")
+  ok
+    [Define ("f",Lambda (["x"],(Let (["x",Var "x"],Block [Lambda ([],Block [Var "x"])]))))]
+    (trans @@ compile_string "(define (f x) (lambda () x))")
+
+test closure_with_class =
+  ok [
+    Class ("Foo",("","Object"),
+	   ["init",["self"],
+	      Let (["self",Var "self"],
+		   Block [Lambda ([],Block [Var "self"])])])] @@
+    trans @@ compile_string "(define-class Foo (Object) ())  (define-method init ((self Foo)) (lambda () self))"
 
