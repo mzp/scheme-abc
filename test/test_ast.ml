1b30754b28b35a3b951c2ce9b0beb36d5b1ed762
mzp <mzpppp@gmail.com>
2008-11-09 23:08:24 +0900

    [REFACTOR]base moudle test

--- test/test_ast.ml
+++ test/test_ast.ml
@@ -8,15 +8,16 @@ let ok x y =
 let ok_stmt x y =
   OUnit.assert_equal ~printer:Ast.to_string_stmt x y
 
-let _ = ("ast module test" >::: [
-  "map" >::
-    (fun () ->
-       ok (Block [Int 42;Int 42;Block [Int 42]]) @@
-	 Ast.map (function Int _ -> Int 42 | e -> e) @@
-	 Block [Int 0; Int 1; Block [Int 3]]);
-  "lift" >::
-    (fun () ->
-       ok_stmt (Expr (Int 42)) @@
-	 lift_stmt (Ast.map (function Int _ -> Int 42 | e -> e)) @@
-	 Ast.Expr (Ast.Int 10))
-	 ]) +> run_test_tt
+let _ = 
+  ("ast module test" >::: [
+     "map" >::
+       (fun () ->
+	  ok (Block [Int 42;Int 42;Block [Int 42]]) @@
+	    Ast.map (function Int _ -> Int 42 | e -> e) @@
+	    Block [Int 0; Int 1; Block [Int 3]]);
+     "lift" >::
+       (fun () ->
+	  ok_stmt (Expr (Int 42)) @@
+	    lift_stmt (Ast.map (function Int _ -> Int 42 | e -> e)) @@
+	    Ast.Expr (Ast.Int 10))
+   ]) +> run_test_tt
--- test/test_base.ml
+++ test/test_base.ml
@@ -1,27 +1,33 @@
 open Base
+open OUnit
 open Util
 
-test range = 
-    assert_equal [1;2;3] @@ range 1 4;
-    assert_equal [] @@ range 1 1;
-    assert_equal [1] @@ range 1 2;
-    assert_equal [] @@ range 1 0
-
-test unfold =
-    let f x = 
-      if x <= 0 then
-	None
-      else
-	Some (x,x-1)
-    in
-      assert_equal [3;2;1] @@ unfold f 3;
-      assert_equal [] @@ unfold f 0;
-      assert_equal [1] @@ unfold f 1
-
-test map_accum_left =
-    assert_equal (0, []) @@ map_accum_left (fun i x->(i+1,x+i)) 0 [];
-    assert_equal (4, [1; 2; 3; 4]) @@ map_accum_left (fun i x->(i+1,x+i)) 0 [1;1;1;1]
-
-test group_by =
-    assert_equal [[1];[2;2];[3]] @@ group_by (==) [1;2;2;3]
-
+let _ =
+  ("base module test" >::: [
+     "range" >::
+       (fun () -> 
+	  ok [1;2;3] @@ range 1 4;
+	  ok [] @@ range 1 1;
+	  ok [1] @@ range 1 2;
+	  ok [] @@ range 1 0);
+     "unfold" >::
+       (fun () ->
+	  let f x = 
+	    if x <= 0 then
+	      None
+	    else
+	      Some (x,x-1)
+	  in
+	    ok [3;2;1] @@ unfold f 3;
+	    ok [] @@ unfold f 0;
+	    ok [1] @@ unfold f 1);
+     "map_accum_left" >::
+       (fun () ->
+	  ok (0, []) @@ 
+	    map_accum_left (fun i x->(i+1,x+i)) 0 [];
+	  ok (4, [1; 2; 3; 4]) @@ 
+	    map_accum_left (fun i x->(i+1,x+i)) 0 [1;1;1;1]);
+     "group_by" >::
+       (fun () ->
+	  ok [[1];[2;2];[3]] @@ group_by (==) [1;2;2;3])
+   ]) +> run_test_tt
