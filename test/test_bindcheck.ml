78301a05b166158b71f6ecabdd9f05c66573430f
mzp <mzpppp@gmail.com>
2008-11-27 09:21:44 +0900

    [UPDATE]I've added a category for bindCheck.ml test

--- test/test_bindcheck.ml
+++ test/test_bindcheck.ml
@@ -57,53 +57,55 @@ let ok_e xs =
 
 let _ =
   ("bindCheck.ml" >::: [
-     "let" >::
-       (fun () ->
-	  ok_e (`Let([node "x",int 42],var "x"));
-	  ok_e (`Let([node "x",int 42],`Call [var "x"])));
-     "let-let-var" >::
-       (fun () ->
-	  ok_e (`Let ([node "x",int 42],
-		      `Let ([node "y",int 10],
-			   var "x"))));
-     "letrec" >::
-       (fun () ->
-	  ok_e (`LetRec([node "xyz",int 42],var "xyz"));
-	  ok_e (`LetRec([node "x",var "x"],var "x")));
-     "lambda" >::
-       (fun () ->
-	  ok_e (`Lambda ([node "x";node "y"],var "x"));
-	  ok_e (`Lambda ([node "x";node "y"],var "y")));
-     "define" >::
-       (fun () ->
-	  ok_s [`Define (node "x",`Block [var "x"])];
-	  ok_s [`Define (node "x",`Block []);
-		`Expr (var "x")]);
-     "external" >::
-       (fun () ->
-	  ok_s [`External (node "x");
-		`Expr (var "x")]);
-     "external-class" >::
-       (fun () ->
-	  ok_s [`ExternalClass (node ("","Object"),[]);
-		`Class (node "Foo",node ("","Object"),[],[])];
-	  ok_s [`ExternalClass (node ("","Object"),[]);
-		`Expr (`New (node ("","Object"),[]))];
-	  ok_s [`ExternalClass (node ("","Object"),[node "f"; node "g"]);
-		`External (node "obj");
-		`Expr (`Invoke (var "obj",node "f",[]))]);
-     "class" >::
-       (fun () ->
-	  ok_s [`ExternalClass (node ("","Object"),[]);
-		`Class (node "Foo",node ("","Object"),[],[]);
-		`Expr (`New (node ("","Foo"),[]))];
-	  ok_s [`ExternalClass (node ("","Object"),[]);
-		`Class (node "Foo",node ("","Object"),[],
-			[(node "f",[],`Block [])]);
-		`External (node "obj");
-		`Expr (`Invoke (var "obj",node "f",[]))];
-	  ok_s [`ExternalClass (node ("","Object"),[]);
-		`External (node "obj");
-		`Class (node "Foo",node ("","Object"),[],
-			[(node "f",[],`Invoke (var "obj",node "f",[]))])])
+     "valid phase" >::: [
+       "let" >::
+	 (fun () ->
+	    ok_e (`Let([node "x",int 42],var "x"));
+	    ok_e (`Let([node "x",int 42],`Call [var "x"])));
+       "let-let-var" >::
+	 (fun () ->
+	    ok_e (`Let ([node "x",int 42],
+			`Let ([node "y",int 10],
+			      var "x"))));
+       "letrec" >::
+	 (fun () ->
+	    ok_e (`LetRec([node "xyz",int 42],var "xyz"));
+	    ok_e (`LetRec([node "x",var "x"],var "x")));
+       "lambda" >::
+	 (fun () ->
+	    ok_e (`Lambda ([node "x";node "y"],var "x"));
+	    ok_e (`Lambda ([node "x";node "y"],var "y")));
+       "define" >::
+	 (fun () ->
+	    ok_s [`Define (node "x",`Block [var "x"])];
+	    ok_s [`Define (node "x",`Block []);
+		  `Expr (var "x")]);
+       "external" >::
+	 (fun () ->
+	    ok_s [`External (node "x");
+		  `Expr (var "x")]);
+       "external-class" >::
+	 (fun () ->
+	    ok_s [`ExternalClass (node ("","Object"),[]);
+		  `Class (node "Foo",node ("","Object"),[],[])];
+	    ok_s [`ExternalClass (node ("","Object"),[]);
+		  `Expr (`New (node ("","Object"),[]))];
+	    ok_s [`ExternalClass (node ("","Object"),[node "f"; node "g"]);
+		  `External (node "obj");
+		  `Expr (`Invoke (var "obj",node "f",[]))]);
+       "class" >::
+	 (fun () ->
+	    ok_s [`ExternalClass (node ("","Object"),[]);
+		  `Class (node "Foo",node ("","Object"),[],[]);
+		  `Expr (`New (node ("","Foo"),[]))];
+	    ok_s [`ExternalClass (node ("","Object"),[]);
+		  `Class (node "Foo",node ("","Object"),[],
+			  [(node "f",[],`Block [])]);
+		  `External (node "obj");
+		  `Expr (`Invoke (var "obj",node "f",[]))];
+	    ok_s [`ExternalClass (node ("","Object"),[]);
+		  `External (node "obj");
+		  `Class (node "Foo",node ("","Object"),[],
+			  [(node "f",[],`Invoke (var "obj",node "f",[]))])])
+     ]
    ]) +> run_test_tt
