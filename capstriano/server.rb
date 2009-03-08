0eab666fd7d35fd677ed7106cbae3befefc3bc42
mzp <mzpppp@gmail.com>
2009-03-08 11:51:14 +0900

    [Capstriano]bufix: shell script(use -ge instead of >)

--- capstriano/server.rb
+++ capstriano/server.rb
@@ -5,5 +5,6 @@ require 'json'
 post '/' do
   if JSON.parse(params[:payload])["repository"]["url"] == 'http://github.com/mzp/scheme-abc' then
     system('git pull && cap deploy:snapshot')
+    system('cap deploy:statics')
   end
 end
