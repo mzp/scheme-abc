a1e75d829afeeacc8f18f7b9af42dfcb60bbe0d0
mzp <mzpppp@gmail.com>
2009-03-13 16:31:47 +0900

    remove auto-generated file from repos

--- base/config.ml
+++
@@ -1,5 +0,0 @@
-(* CAUTION: this is a generated file.  If you edit it, all changes will be lost! *)
-let version = "0.4.1[2009-xx-xx]+auto-import"
-let default_includes = ["/usr/local/lib/habc";"."]
-let default_template = "/usr/local/lib/habc/template.xml"
-let path_sep=":"
