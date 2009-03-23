e0f6802f968748d2e8ad192b42c9560738a6dbcc
mzp <mzpppp@gmail.com>
2009-03-24 07:57:08 +0900

    fix

--- capistrano/server.rb
+++ capistrano/server.rb
@@ -6,7 +6,7 @@ post '/' do
   json = JSON.parse(params[:payload])
   if json["repository"]["url"] == 'http://github.com/mzp/scheme-abc' then
     ENV['REVISION'] = json['after']
-    if File.basename json['ref'] == 'master'
+    if File.basename(json['ref']) == 'master'
       system('twtr up -m "start snapshot build"')
       system('git pull && cap deploy:snapshot')
       system('cap deploy:statics')
