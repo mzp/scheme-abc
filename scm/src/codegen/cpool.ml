30fc235ad7f92cb006773151941b0c51a3e7c38f
mzp <mzpppp@gmail.com>
2009-05-14 07:24:24 +0900

    refactoring cpool

--- scm/src/codegen/cpool.ml
+++ scm/src/codegen/cpool.ml
@@ -25,23 +25,23 @@ type entry = [
 ]
 
 type t = {
-  int: int list;
-  uint: int list;
-  double: float list;
-  string: string list;
-  namespace: namespace list;
-  namespace_set: namespace_set list;
-  multiname: multiname list;
+  int: int ISet.t;
+  uint: int ISet.t;
+  double: float ISet.t;
+  string: string ISet.t;
+  namespace: namespace ISet.t;
+  namespace_set: namespace_set ISet.t;
+  multiname: multiname ISet.t;
 }
 
 let empty =
-  {int           = [];
-   uint          = [];
-   double        = [];
-   string        = [];
-   namespace     = [];
-   namespace_set = [];
-   multiname     = []}
+  {int           = ISet.empty;
+   uint          = ISet.empty;
+   double        = ISet.empty;
+   string        = ISet.empty;
+   namespace     = ISet.empty;
+   namespace_set = ISet.empty;
+   multiname     = ISet.empty}
 
 let ns_name =
   function
@@ -67,26 +67,30 @@ let add_multiname cpool name =
   match name with
       `QName (ns,str) ->
 	{cpool with
-	   string    = (ns_name ns) ^:: str ^:: cpool.string;
-	   namespace = ns ^:: cpool.namespace;
-	   multiname = name ^:: cpool.multiname }
+	   string    = cpool.string
+	               +> ISet.add str
+		       +> ISet.add (ns_name ns);
+	   namespace = ISet.add ns cpool.namespace;
+	   multiname = ISet.add name cpool.multiname }
     | `Multiname (str,ns_set) ->
 	{cpool with
-	   string        = str ^:: (List.map ns_name ns_set ^@ cpool.string);
-	   namespace     = ns_set ^@ cpool.namespace;
-	   namespace_set = ns_set ^:: cpool.namespace_set;
-	   multiname     = name ^:: cpool.multiname }
+	   string        = cpool.string
+	                   +> ISet.add_list (List.map ns_name ns_set)
+			   +> ISet.add str;
+	   namespace     = ISet.add_list ns_set cpool.namespace;
+	   namespace_set = ISet.add ns_set cpool.namespace_set;
+	   multiname     = ISet.add name cpool.multiname }
 
 let add cpool : entry -> t =
   function
       `Int n ->
-	{ cpool with int= n ^:: cpool.int }
+	{ cpool with int= ISet.add n cpool.int }
     | `UInt n ->
-	{ cpool with uint= n ^:: cpool.uint }
+	{ cpool with uint= ISet.add n cpool.uint }
     | `String s ->
-	{ cpool with string = s ^:: cpool.string }
+	{ cpool with string = ISet.add s cpool.string }
     | `Double d ->
-	{ cpool with double = d ^::cpool.double }
+	{ cpool with double = ISet.add d cpool.double }
     | #multiname as m ->
 	add_multiname cpool m
 
@@ -95,15 +99,8 @@ let add cpool : entry -> t =
   assumption:
   - list has only unique element
 *)
-let rec rindex x =
-  function
-      [] ->
-	failwith "empty list(must not happen)"
-    | y::ys ->
-	if x = y then
-	  1 + List.length ys
-	else
-	  rindex x ys
+let rindex x set =
+  1 + ISet.index x set
 
 let index cpool : entry -> int =
   function
@@ -118,7 +115,7 @@ let index cpool : entry -> int =
     | #multiname as m ->
 	rindex m cpool.multiname
 
-let of_namespace ~string (ns : namespace) =
+let of_namespace {string=string} (ns : namespace) =
   let i =
     rindex (ns_name ns) string in
   let kind =
@@ -139,37 +136,28 @@ let of_namespace ~string (ns : namespace) =
 	  0x05 in
     {Abc.kind=kind; namespace_name=i}
 
-let of_namespace_set ~string ~namespace nss =
-  List.map (fun ns -> rindex (of_namespace ~string:string ns) namespace) nss
+let of_namespace_set {namespace=namespace} nss =
+  List.map (fun ns -> rindex ns namespace) nss
 
-let of_multiname ~string ~namespace ~namespace_set : multiname -> Abc.multiname =
+let of_multiname {namespace=namespace; namespace_set=namespace_set; string=string} : multiname -> Abc.multiname =
   function
       `QName (ns,s) ->
-	Abc.QName (rindex (of_namespace ~string:string ns) namespace, rindex s string)
+	Abc.QName (rindex ns namespace, rindex s string)
     | `Multiname (s,nss) ->
-	Abc.Multiname (rindex s string,rindex (of_namespace_set ~string:string ~namespace:namespace nss) namespace_set)
+	Abc.Multiname (rindex s string,rindex nss namespace_set)
 
 let to_abc cpool =
-  let int,uint,double,str,ns,nss =
-    cpool.int,
-    cpool.uint,
-    cpool.double,
-    cpool.string,
-    cpool.namespace,
-    cpool.namespace_set in
-  let ns' =
-    List.map (of_namespace ~string:str) ns in
-  let nss' =
-    List.map (of_namespace_set ~namespace:ns' ~string:str) nss in
-  let mname =
-    List.map (of_multiname ~string:str ~namespace:ns' ~namespace_set:nss') cpool.multiname in
-    { Abc.int           = List.rev int;
-      Abc.uint          = List.rev uint;
-      Abc.double        = List.rev double;
-      Abc.string        = List.rev str;
-      Abc.namespace     = List.rev ns';
-      Abc.namespace_set = List.rev nss';
-      Abc.multiname     = List.rev mname
-    }
-
-
+  { Abc.int           = ISet.to_list cpool.int;
+    Abc.uint          = ISet.to_list cpool.uint;
+    Abc.double        = ISet.to_list cpool.double;
+    Abc.string        = ISet.to_list cpool.string;
+    Abc.namespace     = cpool.namespace
+                        +> ISet.to_list
+                        +> List.map (of_namespace cpool);
+    Abc.namespace_set = cpool.namespace_set
+                        +> ISet.to_list
+                        +> List.map (of_namespace_set cpool);
+    Abc.multiname     = cpool.multiname
+                        +> ISet.to_list
+                        +> List.map (of_multiname cpool)
+  }
