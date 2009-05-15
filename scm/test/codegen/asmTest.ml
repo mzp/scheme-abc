a73969daa18bb4904c8f6dd7a0e388c051872a74
mzp <mzpppp@gmail.com>
2009-05-16 07:41:51 +0900

    add NewFunction & its test

--- scm/test/codegen/asmTest.ml
+++ scm/test/codegen/asmTest.ml
@@ -17,29 +17,49 @@ let empty = {
 
 let _ =
   ("asm.ml" >::: [
-     "top level method only" >:::
+     ("top level method only" >:::
        let { abc_cpool =cpool; method_info = info; method_body = body } =
 	 assemble {empty with
 		     method_name = `QName (`Namespace "","main");
 		     instructions = [PushString "a"; PushString "b"; PushString "c"]} in
-
-[
-	 "cpool" >::
-	   (fun () ->
-	      assert_equal {Abc.empty_cpool with
-			      Abc.string    = ["a";"b";"c";"main";""];
-			      namespace = [{Abc.kind = 8; Abc.namespace_name = 5}];
-			      multiname = [Abc.QName (1, 4)]}
-		cpool);
-	 "instruction" >::
-	   (fun () ->
-	      assert_equal [u8 0x2c;u30 1;
-			    u8 0x2c;u30 2;
-			    u8 0x2c;u30 3]
-		(List.hd body).Abc.code);
-	 "usage" >::
-	   (fun () ->
-	      assert_equal 3 (List.hd body).Abc.max_stack;
-	      assert_equal 0 (List.hd body).Abc.max_scope_depth)
-     ]]) +> run_test_tt
+	 [
+	   "cpool" >::
+	     (fun () ->
+		assert_equal {Abc.empty_cpool with
+				Abc.string    = ["a";"b";"c";"main";""];
+				namespace = [{Abc.kind = 8; Abc.namespace_name = 5}];
+				multiname = [Abc.QName (1, 4)]}
+		  cpool);
+	   "instruction" >::
+	     (fun () ->
+		assert_equal [u8 0x2c;u30 1;
+			      u8 0x2c;u30 2;
+			      u8 0x2c;u30 3]
+		  (List.hd body).Abc.code);
+	   "usage" >::
+	     (fun () ->
+		assert_equal 3 (List.hd body).Abc.max_stack;
+		assert_equal 0 (List.hd body).Abc.max_scope_depth)
+	 ]);
+     ("method nested" >:::
+      let {abc_cpool =cpool; method_info = info; method_body = body } =
+	assemble {empty with
+		    instructions = [
+		      PushString "foo";
+		      NewFunction {empty with instructions=[PushString "bar"]};
+		      NewFunction  {empty with instructions=[PushString "baz"]}
+		    ]}
+      in
+	[
+	  "cpool" >::
+	    (fun () ->
+	       assert_equal
+		 (List.sort compare ["foo";"bar";"baz";""]) @@
+		 (List.sort compare cpool.Abc.string));
+	  "method count" >::
+	    (fun () ->
+	       assert_equal 3 @@ List.length info;
+	       assert_equal 3 @@ List.length body)
+	])
+   ]) +> run_test_tt
 
