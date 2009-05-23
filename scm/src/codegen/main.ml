e670eb4ddf7cd54b1d7bc5c1470661baa48553a6
mzp <mzpppp@gmail.com>
2009-05-23 09:21:50 +0900

    add Codegen.main

---
+++ scm/src/codegen/main.ml
@@ -0,0 +1,19 @@
+open Base
+
+
+let generate program =
+  program
+  +> Module.of_ast
+  +> Binding.of_module
+  +> curry Codegen.generate
+  +> Abc.to_bytes
+  +> Bytes.to_int_list
+
+let output ch program =
+  program
+  +> Module.of_ast
+  +> Binding.of_module
+  +> curry Codegen.generate
+  +> Abc.to_bytes
+  +> Bytes.output_bytes ch
+
