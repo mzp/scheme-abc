4d073e6ec728311d376b83beff8d7c1060ce6ebc
mzp <mzpppp@gmail.com>
2009-09-13 20:29:08 +0900

    bugfix: test driver

--- scm/example/module/run.sh
+++ scm/example/module/run.sh
@@ -1,7 +1,7 @@
 ROOT=$1
 cd $(dirname $0)
 ${ROOT}/scm/habc-scm -c -I "${ROOT}/lib/habc:." fact.scm
-${ROOT}/habc-scm -I "${ROOT}/lib/habc/:."  fact.ho  main.scm
+${ROOT}/scm/habc-scm -I "${ROOT}/lib/habc/:."  fact.ho  main.scm
 res=$(avmplus a.abc)
 
 if [[ $res == 3628800 ]]; then
