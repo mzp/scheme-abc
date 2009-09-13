69691b4d727b82953bb62f11615f7bf96f9376d5
mzp <mzpppp@gmail.com>
2009-09-13 19:05:13 +0900

    add type annot for abc module

--- swflib/abc.ml
+++ swflib/abc.ml
@@ -3,5 +3,10 @@ module C = Compile.Make(HighInst)
 include AbcType
 include MethodType
 
-let asm = A.to_bytes
-let compile = C.to_abc
+let asm : LowInst.t abc -> Bytes.t list = A.to_bytes
+let compile : Cpool.multiname list -> HighInst.s method_ -> LowInst.t abc = C.to_abc
+
+let output : out_channel -> Bytes.t list -> unit =
+  Bytes.output_bytes
+
+
