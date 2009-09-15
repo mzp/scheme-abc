0481c4f70b8be97131750a2f52bad3799002aa43
mzp <mzpppp@gmail.com>
2009-09-15 20:23:23 +0900

    add bytesIn module

---
+++ swflib/bytesIn.mli
@@ -0,0 +1,6 @@
+val u8  : int Stream.t -> int
+val u16 : int Stream.t -> int
+val s24 : int Stream.t -> int
+val u30 : int Stream.t -> int
+val u32 : int Stream.t -> int32
+val d64 : int Stream.t -> float
