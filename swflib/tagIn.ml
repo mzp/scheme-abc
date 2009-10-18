94a36d182cc1728271baf1eb47dc1ab1ede12a75
mzp <mzpppp@gmail.com>
2009-10-18 15:15:51 +0900

    remove dead code

--- swflib/tagIn.ml
+++ swflib/tagIn.ml
@@ -1,14 +1,6 @@
 open Base
 open SwfBaseIn
 
-let tag n s =
-  match Stream.peek s with
-      Some m when n = m ->
-	Stream.junk s;
-	()
-    | None | Some _ ->
-	raise Stream.Failure
-
 let of_base tag stream =
   match tag with
       0 ->
